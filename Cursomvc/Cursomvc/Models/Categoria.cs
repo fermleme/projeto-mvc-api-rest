a6aa266ae62a4a6e4aa1f1797efca7d8f83d6a04
Fernando Leme <fermleme@hotmail.com>
2020-07-15 13:32:59 -0300

    Projeto MVC com EntityFrameworkCore

---
+++ Cursomvc/Cursomvc/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursomvc.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Display(Name ="Descrição")]
+        [Required(ErrorMessage = "O campo Descrição é Obrigatório")]
+        //[Range(1, 50, ErrorMessage = "A Descricao deve estar entre 1 a 40 caracteres")]
+        public string Descricao {get; set;}
+
+        public List<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ Cursomvc/Cursomvc/Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cursomvc.Models
+{
+    public class Produto
+    {
+	
+
+			public int Id { get; set; }
+		    [Display(Name = "Descrição")]
+			[Required(ErrorMessage = "O campo Descrição é Obrigatório")]
+			//[Range(1, 50, ErrorMessage = "A Descricao deve estar entre 1 a 50 caracteres")]
+		    public string Descricao { get; set; }
+		    [Range(1, 50, ErrorMessage = "A quantidade deve estar entre 1 a 50")] 
+			public int Quantidade { get; set; }
+			public int CategoriaId { get; set; }
+			public Categoria Categoria { get; set; }
+		
+	}
+}
